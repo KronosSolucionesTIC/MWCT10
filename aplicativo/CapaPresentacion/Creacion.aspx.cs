dee8216d3ae6b27b0d9dfe8e62b51e6e4a90c26d
Kronos <desarrollador.grupoempresarial@gmail.com>
2019-05-07 11:24:42 -0500

    Cliente y usuario tambien en menu creacion tareas, correcion imagenes login

--- aplicativo/CapaPresentacion/Creacion.aspx.cs
+++ aplicativo/CapaPresentacion/Creacion.aspx.cs
@@ -13,6 +13,15 @@ namespace CapaPresentacion
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Consulta ci = new Consulta();               //Crea una instancia de clase
+            string usu = Convert.ToString(Session["Login"]); //Lee la variable Session
+            ci.Usuario = usu;                           //Pasa el valor de usuario
+            string Id = ci.getId();                     //Pasa el metodo getId para validar si existe el usuario     
+            string cli = ci.getCliente();               //Pasa el metodo getId para validar si existe el usuario     
+            ci.Cliente = Id;                            //Pasa el valor de la lista
+            usuario.Text = usu;                         //Pone nombre usuario
+            cliente.Text = cli;                         //Pone nombre cliente
+
             if (!IsPostBack)
             {
                 llenar_marca();         //Pasa funcion para llenar lista
--- aplicativo/CapaPresentacion/Creacion.aspx.designer.cs
+++ aplicativo/CapaPresentacion/Creacion.aspx.designer.cs
@@ -13,6 +13,24 @@ namespace CapaPresentacion {
     public partial class WebForm1 {
         
         /// <summary>
+        /// Control cerrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cerrar;
+        
+        /// <summary>
+        /// Control cliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label cliente;
+        
+        /// <summary>
         /// Control usuario.
         /// </summary>
         /// <remarks>
